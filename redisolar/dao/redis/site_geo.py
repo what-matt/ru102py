77e44edffe4082b81e022b24a12e0757c18533fc
Matt <matt.riquelme.a@gmail.com>
2021-02-21 18:01:06 -0300

    cleaning

--- redisolar/dao/redis/site_geo.py
+++ redisolar/dao/redis/site_geo.py
@@ -54,11 +54,11 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
 
     def _find_by_geo_with_capacity(self, query: GeoQuery, **kwargs) -> Set[Site]:
         # START Challenge #5
+        # Your task: Get the sites matching the GEO query.
         site_ids = self.redis.georadius(
             self.key_schema.site_geo_key(), query.coordinate.lng, query.coordinate.lat,
             query.radius, query.radius_unit.value
         )
-        # Your task: Get the sites matching the GEO query.
         # END Challenge #5
 
         p = self.redis.pipeline(transaction=False)
@@ -74,14 +74,7 @@ class SiteGeoDaoRedis(SiteGeoDaoBase, RedisDaoBase):
             p.zscore(self.key_schema.capacity_ranking_key(), site_id)
 
         scores = dict(zip(site_ids, p.execute()))
-
-        # p.hgetall(self.key_schema.site_hash_key(site_id))
         # END Challenge #5
-
-        # Delete the next lines after you've populated a `site_ids`
-        # and `scores` variable.
-        # site_ids: List[str] = []
-        # scores: Dict[str, float] = {}
 
         for site_id in site_ids:
             if scores[site_id] and scores[site_id] > CAPACITY_THRESHOLD:
