8bcf43a48e24ab0c2693ff292383c85ac56490fb
Matt <matt.riquelme.a@gmail.com>
2021-03-01 19:26:58 -0300

    challenge 8

--- redisolar/dao/redis/meter_reading.py
+++ redisolar/dao/redis/meter_reading.py
@@ -2,7 +2,7 @@ from redisolar.dao.base import MeterReadingDaoBase
 from redisolar.dao.redis.base import RedisDaoBase
 from redisolar.dao.redis.capacity_report import CapacityReportDaoRedis
 from redisolar.dao.redis.feed import FeedDaoRedis
-from redisolar.dao.redis.metric import MetricDaoRedis
+from redisolar.dao.redis.metric_timeseries import MetricDaoRedisTimeseries
 from redisolar.models import MeterReading
 
 # Uncomment for Challenge #3
@@ -13,7 +13,7 @@ from redisolar.models import MeterReading
 class MeterReadingDaoRedis(MeterReadingDaoBase, RedisDaoBase):
     """MeterReadingDaoRedis persists MeterReading models to Redis."""
     def add(self, meter_reading: MeterReading, **kwargs) -> None:
-        MetricDaoRedis(self.redis, self.key_schema).insert(meter_reading, **kwargs)
+        MetricDaoRedisTimeseries(self.redis, self.key_schema).insert(meter_reading, **kwargs)
         CapacityReportDaoRedis(self.redis, self.key_schema).update(meter_reading, **kwargs)
         FeedDaoRedis(self.redis, self.key_schema).insert(meter_reading, **kwargs)
 
